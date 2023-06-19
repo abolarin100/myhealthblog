5da4f55d011af95df71d208ac5e889096c2a0e99
abolarin100 <abolarin100@gmail.com>
2023-06-19 09:15:29 -0700

    made another update

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -12,7 +12,7 @@ const Create = () => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    fetch('http://localhost:8000/blogs/', {
+    fetch('https://my-json-server.typicode.com/abolarin100/data', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
